9c4e71013666bbdc0ed485024c3a6de49b4e8b6b
Dveneziale <dominicveneziale24@gmail.com>
2025-02-13 18:59:12 -0500

    added form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,355 +1,218 @@
-<div class="center-wrapper">
-  <!--breaks before the start of the code so that it doesnt start at the very top of the page-->
-  <br>
-  <br>
-  <br>
-  <br>
+<form action="DATABASE_URL" method="POST">
+  <div class="center-wrapper">
+    <br><br><br><br>
 
-  <!--This is the input box that will be used to get peoples names-->
-  <input 
+    <!-- Name Input -->
+    <input 
       type="text" 
+      name="name"
       placeholder="Name"
       class="input place-self-center input-bordered w-full max-w-xs center"
+      required
+    /><br>
+<!---->
+    <input 
+      type="text" 
+      name="match_number"
+      placeholder="match"
+      class="input place-self-center input-bordered w-full max-w-xs center"
+      required
+    /><br>
+
+    <!-- Team Dropdown -->
+    <select name="team_number" class="select select-bordered w-full max-w-xs" required>
+      <option disabled selected>Team</option>
+      <option value="1">Team 1</option>
+      <option value="2">Team 2</option>
+      <option value="3">Team 3</option>
+      <option value="4">Team 4</option>
+      <option value="5">Team 5</option>
+    </select>
+    <br>
+
+    <!-- Auton Section -->
+    <header>
+      <h1 class="center-header">Auton</h1>
+    </header>
+
+    <label class="label cursor-pointer">
+      <span class="label-text">Did they leave community?</span>
+      <input type="checkbox" name="auton_left_community" class="toggle" />
+    </label>
+
+    <header>
+      <h1 class="center-header">4th Reef Scored</h1>
+    </header>
+  
+    <button type="button" class="btn">-</button>
+    <input type="number" name="autonFourthCoral" value="0" class="input w-full max-w-xs" />
+    <button type="button" class="btn">+</button>
+
+    <header>
+      <h1 class="center-header">3rd Reef Scored</h1>
+    </header>
+
+    <button type="button" class="btn">-</button>
+    <input type="number" name="autonThirdCoral" value="0" class="input w-full max-w-xs" />
+    <button type="button" class="btn">+</button>
+
+    <header>
+      <h1 class="center-header">2nd Reef Scored</h1>
+    </header>
+
+    <button type="button" class="btn">-</button>
+    <input type="number" name="autonSecondCoral" value="0" class="input w-full max-w-xs" />
+    <button type="button" class="btn">+</button>
+
+    <header>
+      <h1 class="center-header">1st Reef Scored</h1>
+    </header>
+
+    <button type="button" class="btn">-</button>
+    <input type="number" name="autonFirstCoral" value="0" class="input w-full max-w-xs" />
+    <button type="button" class="btn">+</button>
+
+    <label class="label cursor-pointer">
+      <span class="label-text">Did they move algae?</span>
+      <input type="checkbox" name="auton_moved_algae" class="toggle" />
+    </label>
+
+    <header>
+      <h1 class="center-header">Processor Scored</h1>
+    </header>
+
+    <button type="button" class="btn">-</button>
+    <input type="number" name="autonProcessor" value="0" class="input w-full max-w-xs" />
+    <button type="button" class="btn">+</button>
+
+    <header>
+      <h1 class="center-header">Algae Scored in Barge</h1>
+    </header>
+
+    <button type="button" class="btn">-</button>
+    <input type="number" name="autonBarge" value="0" class="input w-full max-w-xs" />
+    <button type="button" class="btn">+</button>
+
+    <!-- Teleop Section -->
+    <header>
+      <h1 class="center-header">Teleop</h1>
+    </header>
+
+    <header>
+      <h1 class="center-header">4th Reef Scored</h1>
+    </header>
+
+    <button type="button" class="btn">-</button>
+    <input type="number" name="teleopFourthCoral" value="0" class="input w-full max-w-xs" />
+    <button type="button" class="btn">+</button>
+
+    <header>
+      <h1 class="center-header">3rd Reef Scored</h1>
+    </header>
+
+    <button type="button" class="btn">-</button>
+    <input type="number" name="teleopThirdCoral" value="0" class="input w-full max-w-xs" />
+    <button type="button" class="btn">+</button>
+
+    <header>
+      <h1 class="center-header">2nd Reef Scored</h1>
+    </header>
+
+    <button type="button" class="btn">-</button>
+    <input type="number" name="teleopSecondCoral" value="0" class="input w-full max-w-xs" />
+    <button type="button" class="btn">+</button>
+
+    <header>
+      <h1 class="center-header">1st Reef Scored</h1>
+    </header>
+
+    <button type="button" class="btn">-</button>
+    <input type="number" name="teleopFirstCoral" value="0" class="input w-full max-w-xs" />
+    <button type="button" class="btn">+</button>
+
+    <header>
+      <h1 class="center-header">Processor Scored</h1>
+    </header>
+
+    <button type="button" class="btn">-</button>
+    <input type="number" name="teleopProcessor" value="0" class="input w-full max-w-xs" />
+    <button type="button" class="btn">+</button>
+
+    <header>
+      <h1 class="center-header">Algae Scored in Barge</h1>
+    </header>
+
+    <button type="button" class="btn">-</button>
+    <input type="number" name="teleopBarge" value="0" class="input w-full max-w-xs" />
+    <button type="button" class="btn">+</button>
+
+    <label class="label cursor-pointer">
+      <span class="label-text">Did they break?</span>
+      <input type="checkbox" name="did_break" class="toggle" />
+    </label>
+
+    <header>
+      <h1>Did they play defense? How well?</h1>
+    </header>
+
+    <input 
+      type="text" 
+      name="defense"
+      placeholder="Defense?"
+      class="input place-self-center input-bordered w-full max-w-xs center"
+    /><br>
+
+    <label class="label cursor-pointer">
+      <span class="label-text">Did they shallow climb?</span>
+      <input type="checkbox" name="shallow_climb" class="toggle" />
+    </label>
+
+    <label class="label cursor-pointer">
+      <span class="label-text">Did they deep climb?</span>
+      <input type="checkbox" name="deep_climb" class="toggle" />
+    </label>
+
+    <header>
+      <h1>Any extra notes</h1>
+    </header>
+    <input 
+    type="notes" 
+    name="notes"
+    placeholder="notes?"
+    class="input place-self-center input-bordered w-full max-w-xs center"
   /><br>
 
-  <!--this is the dropdown that will be used to select the team number that you are scouting-->
-  <select class="select select-bordered w-full max-w-xs">
-    <option disabled selected>team</option>
-    <option>team 2</option>
-    <option>team 1</option>
-    <option>team 1</option>
-    <option>team 1</option>
-    <option>team 1</option>
-    <option>team 1</option>
-    <option>team 1</option>
-    <option>team 1</option>
-    <option>team 1</option>
-    <option>team 1</option>
-    <option>team 1</option>
-    <option>team 1</option>
-    <option>team 1</option>
-    <option>team 1</option>
-    <option>team 1</option>
-    <option>team 1</option>
-    <option>team 1</option>
-    <option>team 1</option>
-    <option>team 1</option>
-  </select>
-  <br>
-
-  <!--this is the division to the auton section of the code -->
-  <header>
-    <h1 class="center-header">Auton</h1>
-  </header>
-  <!--put if they left community here YES/NO -->
-
-  <label class="label cursor-pointer">
-    <span class="label-text">did they leave community</span>
-    <input type="checkbox" class="toggle" checked="checked" />
-  </label>
-  <!--put if they left community here YES/NO -->
-
-  <!--to describe what the button below does-->
-  <header>
-    <h1 class="center-header">4th reef scored</h1>
-  <header>
-  
-  <!--for the auton fourth coral subtraction button-->
-  <button on:click={() => {
-    autonFourthCoral--
-  }}
-   class="btn">-</button>
-
-   <!--this is to show the current value of the number-->
-    <input type="text" value={autonFourthCoral} class="input w-full max-w-xs" />
-
-    <!--onclick to add to autonfourth coral-->
-    <button on:click={() => {
-      autonFourthCoral++
-    }} class="btn">+</button>
-      <!--to describe what the button below does-->
-  <header>
-    <h1 class="center-header">3rd reef scored</h1>
-  <header>
-  
-  <!--for the auton fourth coral subtraction button-->
-  <button on:click={() => {
-    autonThirdCoral--
-  }}
-   class="btn">-</button>
-
-   <!--this is to show the current value of the number-->
-    <input type="text" value={autonThirdCoral} class="input w-full max-w-xs" />
-
-    <!--onclick to add to autonfourth coral-->
-    <button on:click={() => {
-      autonThirdCoral++
-    }} class="btn">+</button>
-
-<header>
-  <h1 class="center-header">2nd reef scored</h1>
-<header>
-
-<!--for the auton fourth coral subtraction button-->
-<button on:click={() => {
-  autonSecondCoral--
-}}
- class="btn">-</button>
-
- <!--this is to show the current value of the number-->
-  <input type="text" value={autonSecondCoral} class="input w-full max-w-xs" />
-
-
-  <!--onclick to add to autonfourth coral-->
-  <button on:click={() => {
-    autonSecondCoral++
-  }} class="btn">+</button>
-
-  
-
-    <!--to describe what the button below does-->
-<header>
-  <h1 class="center-header">1st reef scored</h1>
-<header>
-
-<!--for the auton fourth coral subtraction button-->
-<button on:click={() => {
-  autonFirstCoral--
-}}
- class="btn">-</button>
-
- <!--this is to show the current value of the number-->
-  <input type="text" value={autonFirstCoral} class="input w-full max-w-xs" />
-  
-
-  <!--onclick to add to autonfourth coral-->
-  <button on:click={() => {
-    autonFirstCoral++
-  }} class="btn">+</button>
-
-  <!--did they move algae?-->
-  <label class="label cursor-pointer">
-    <span class="label-text">did they move algae?</span>
-    <input type="checkbox" class="toggle" checked="checked" />
-  </label>
-
-  <header>
-    <h1 class="center-header">processor scored</h1>
-  <header>
-  <!--algae put into processor-->
-  <button on:click={() => {
-    autonProcessor--
-  }}
-   class="btn">-</button>
-
-   <!--this is to show the current value of the number-->
-    <input type="text" value={autonProcessor} class="input w-full max-w-xs" />
-
-    <!--onclick to add to autonfourth coral-->
-    <button on:click={() => {
-      autonProcessor++
-    }} class="btn">+</button>
-
-    
-  <header>
-    <h1 class="center-header">algae scored in Barge</h1>
-  <header>
-  <!--algae put into processor-->
-  <button on:click={() => {
-    autonBarge
-  }}
-   class="btn">-</button>
-
-   <!--this is to show the current value of the number-->
-    <input type="text" value={autonBarge} class="input w-full max-w-xs" />
-
-    <!--onclick to add to autonfourth coral-->
-    <button on:click={() => {
-      autonBarge++
-    }} class="btn">+</button>
-
-<header>
-  <h1 class="center-header">Teleop</h1>
-<header>
-
-   <!--to describe what the button below does-->
-   <header>
-    <h1 class="center-header">4th reef scored</h1>
-  <header>
-  
-  <!--for the auton fourth coral subtraction button-->
-  <button on:click={() => {
-    teleopFourthCoral--
-  }}
-   class="btn">-</button>
-
-   <!--this is to show the current value of the number-->
-    <input type="text" value={teleopFourthCoral} class="input w-full max-w-xs" />
-
-    <!--onclick to add to autonfourth coral-->
-    <button on:click={() => {
-      teleopFourthCoral++
-    }} class="btn">+</button>
-      <!--to describe what the button below does-->
-  <header>
-    <h1 class="center-header">3rd reef scored</h1>
-  <header>
-  
-  <!--for the auton fourth coral subtraction button-->
-  <button on:click={() => {
-    teleopThirdCoral--
-  }}
-   class="btn">-</button>
-
-   <!--this is to show the current value of the number-->
-    <input type="text" value={autonThirdCoral} class="input w-full max-w-xs" />
-
-    <!--onclick to add to autonfourth coral-->
-    <button on:click={() => {
-      teleopThirdCoral++
-    }} class="btn">+</button>
-
-<header>
-  <h1 class="center-header">2nd reef scored</h1>
-<header>
-
-<!--for the auton fourth coral subtraction button-->
-<button on:click={() => {
-  teleopSecondCoral--
-}}
- class="btn">-</button>
-
- <!--this is to show the current value of the number-->
-  <input type="text" value={teleopSecondCoral} class="input w-full max-w-xs" />
-
-
-  <!--onclick to add to autonfourth coral-->
-  <button on:click={() => {
-    teleopSecondCoral++
-  }} class="btn">+</button>
-
-  
-
-    <!--to describe what the button below does-->
-<header>
-  <h1 class="center-header">1st reef scored</h1>
-<header>
-
-<!--for the auton fourth coral subtraction button-->
-<button on:click={() => {
-  teleopFirstCoral--
-}}
- class="btn">-</button>
-
- <!--this is to show the current value of the number-->
-  <input type="text" value={teleopFirstCoral} class="input w-full max-w-xs" />
-  
-
-  <!--onclick to add to autonfourth coral-->
-  <button on:click={() => {
-    teleopFirstCoral++
-  }} class="btn">+</button>
-
-<header>
-  <h1 class="center-header">processor scored</h1>
-<header>
-<!--algae put into processor-->
-<button on:click={() => {
-  teleopProcessor--
-}}
- class="btn">-</button>
-
- <!--this is to show the current value of the number-->
-  <input type="text" value={autonProcessor} class="input w-full max-w-xs" />
-
-  <!--onclick to add to autonfourth coral-->
-  <button on:click={() => {
-    teleopProcessor++
-  }} class="btn">+</button>
-
-  
-<header>
-  <h1 class="center-header">algae scored in Barge</h1>
-<header>
-<!--algae put into processor-->
-<button on:click={() => {
-  teleopBarge--
-}}
- class="btn">-</button>
-
- <!--this is to show the current value of the number-->
-  <input type="text" value={teleopBarge} class="input w-full max-w-xs" />
-
-  <!--onclick to add to autonfourth coral-->
-  <button on:click={() => {
-    teleopBarge++
-  }} class="btn">+</button>
-
-  <!--did they break-->
-  <label class="label cursor-pointer">
-    <span class="label-text">did they break?</span>
-    <input type="checkbox" class="toggle" checked="checked" />
-  </label>
-
-  <!--did they play defense, did they play it well?-->
-<header>
-  <h1> did they play defense, did they play it well?<br></header>
-  <input 
-  type="text" 
-  placeholder="defense?"
-  class="input place-self-center input-bordered w-full max-w-xs center"
-
-/><br>
-
- <!--did they shallow climb-->
- <label class="label cursor-pointer">
-  <span class="label-text">did they shallow climb?</span>
-  <input type="checkbox" class="toggle" checked="checked" />
-</label>
-
-<!--did they deep climb-->
-<label class="label cursor-pointer">
-  <span class="label-text">did they deep climb?</span>
-  <input type="checkbox" class="toggle" checked="checked" />
-</label>
-
-
-
-
-</div>
-<!--this is to declare variables to be used for counters-->
-<script>
-let autonFourthCoral = 0;
-let autonThirdCoral = 0;
-let autonSecondCoral=0;
-let autonFirstCoral=0;
-let autonProcessor=0;
-let autonBarge=0;
-let teleopFirstCoral=0;
-let teleopSecondCoral=0;
-let teleopThirdCoral = 0;
-let teleopFourthCoral=0
-let teleopBarge=0;
-let teleopProcessor=0;
-</script>
+    <br>
+    <button type="submit" class="btn">Submit</button>
+  </div>
+</form>
 
 <style>
-h1 {
-  color: goldenrod;
-  font-family: "Comic Sans MS", cursive;
+input[type="number"]::-webkit-inner-spin-button, 
+input[type="number"]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
 }
 
-.center-wrapper {
-  text-align: center; /* Centers all child elements horizontally */
+input[type="number"] {
+  -moz-appearance: textfield; /* Firefox */
 }
 
-.center-header {
-  margin-top: 20px; /* Adds some 
-  spacing between the dropdown and the header */
-}
+  h1 {
+    color: goldenrod;
+    font-family: "Comic Sans MS", cursive;
+  }
 
-.center {
-  display: inline-block; /* Ensures the input remains block-like for styling */
-  border: 3px solid lightpink;
-}
+  .center-wrapper {
+    text-align: center;
+  }
+
+  .center-header {
+    margin-top: 20px;
+  }
+
+  .center {
+    display: inline-block;
+    border: 3px solid lightpink;
+  }
 </style>
