679d943feeb847888913a7cc10de96877c7712e8
Dveneziale <dominicveneziale24@gmail.com>
2025-10-16 18:55:06 -0400

    rampriot

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,66 +21,7 @@
 
     <!-- Team Dropdown -->
     <select name="team_number" class="select select-bordered w-full max-w-xs" required>
-      <option value="11">11 MORT  </option>
-        <option value="25">25 Raider Robotix  </option>
-        <option value="56">56 R.O.B.B.E.  </option>
-        <option value="75">75 RoboRaider  </option>
-        <option value="102">102 The Gearheads  </option>
-        <option value="103">103 Cybersonics  </option>
-        <option value="193">193 MORT Beta  </option>
-        <option value="222">222 Tigertrons  </option>
-        <option value="223">223 Xtreme Heat  </option>
-        <option value="272">272 Cyber Crusaders  </option>
-        <option value="293">293 SPIKE  </option>
-        <option value="316">316 LUNATECS  </option>
-        <option value="321">321 RoboLancers   </option>
-        <option value="341">341 Miss Daisy  </option>
-        <option value="365">365 Miracle Workerz  </option>
-        <option value="484">484 Roboforce   </option>
-        <option value="555">555 Montclair Robotics  </option>
-        <option value="708">708 Hatters Robotics  </option>
-        <option value="834">834 SparTechs   </option>
-        <option value="1089">1089 Team Mercury  </option>
-        <option value="1168">1168 Malvern Robotics  </option>
-        <option value="1218">1218 SCH Robotics  </option>
-        <option value="1257">1257 Parallel Universe  </option>
-        <option value="1391">1391 The Metal Moose  </option>
-        <option value="1403">1403 Team 1403 Cougar Robotics  </option>
-        <option value="1640">1640 Sab-BOT-age  </option>
-        <option value="1676">1676 The Pascack PI-oneers  </option>
-        <option value="1712">1712 Dawgma   </option>
-        <option value="1807">1807 Redbird Robotics  </option>
-        <option value="1811">1811 FRESH  </option>
-        <option value="1923">1923 The MidKnight Inventors  </option>
-        <option value="2458">2458 Team Chaos  </option>
-        <option value="2495">2495 Hive Mind  </option>
-        <option value="2539">2539 Krypton Cougars</option>
-        <option value="2554">2554 The Warhawks   </option>
-        <option value="2590">2590 Nemesis  </option>
-        <option value="2607">2607 The Fighting RoboVikings  </option>
-        <option value="2720">2720 Red Watch Robotics  </option>
-        <option value="2722">2722 Charge Robotics  </option>
-        <option value="3142">3142 Aperture  </option>
-        <option value="3314">3314 Mechanical Mustangs  </option>
-        <option value="3637">3637 The Daleks  </option>
-        <option value="4342">4342 Demon Robotics  </option>
-        <option value="4361">4361 Roxbotix  </option>
-        <option value="4373">4373 RooBotics  </option>
-        <option value="4653">4653 Ironmen Robotics  </option>
-        <option value="5181">5181 Explorer Robotics   </option>
-        <option value="5401">5401 Fightin' Robotic Owls  </option>
-        <option value="5684">5684 Titans of Tech - Thrive Charter School  </option>
-        <option value="5895">5895 Peddie Robotics   </option>
-        <option value="5992">5992 Pirates  </option>
-        <option value="6808">6808 William Tennent Robotics   </option>
-        <option value="7045">7045 MCCrusaders   </option>
-        <option value="7414">7414 RetroRobotics   </option>
-        <option value="8513">8513 Sisters 1st  </option>
-        <option value="9015">9015 Questionable Engineering  </option>
-        <option value="9094">9094 The Earthquakers  </option>
-        <option value="9416">9416 International Operatives of World Affairs  </option>
-        <option value="10143">10143 Northeast High School Philadelphia Robotics Team   </option>
-        <option value="10157">10157 Roman Robotics  </option>
+      <option value = "834"> 834 Spartechs </option>
     </select>
     <br>
 
